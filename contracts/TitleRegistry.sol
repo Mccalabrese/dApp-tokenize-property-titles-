1573be44e615a0db6fddaefcc012a75a74719e79
Mccalabrese <138624063+Mccalabrese@users.noreply.github.com>
2024-02-29 20:37:03 -0600

    Update TitleRegistry.sol

--- contracts/TitleRegistry.sol
+++ contracts/TitleRegistry.sol
@@ -1,9 +1,22 @@
-pragma solidity ^0.5.5;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract TitleRegistry is ERC721Full {
-    constructor() public ERC721Full("TitleRegistryToken", "TIT") {}
+
+
+contract TitleRegistry is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    constructor() {
+        _disableInitializers();
+    }
 
     struct Title {
         string propertyOwner;
@@ -16,6 +29,20 @@ contract TitleRegistry is ERC721Full {
 
     event Sale(uint256 tokenId, uint256 saleValue, string reportURI, string titleJson);
     
+    function initialize(address initialOwner) initializer public {
+        __ERC721_init("Title Registry Token", "TIT");
+        __ERC721Enumerable_init();
+        __ERC721URIStorage_init();
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+
     function imageUri(
         uint256 tokenId
 
@@ -29,18 +56,24 @@ contract TitleRegistry is ERC721Full {
         string memory name,
         string memory Jurisdiction,
         uint256 initialSaleValue,
-        string memory tokenURI,
+        string memory titleURI,
         string memory tokenJSON
     ) public returns (uint256) {
-        uint256 tokenId = totalSupply();
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(owner, newItemId);
+        _setTokenURI(newItemId, titleURI);
 
-        _mint(owner, tokenId);
-        _setTokenURI(tokenId, tokenURI);
+        savedTitles[newItemId] = Title(name, Jurisdiction, initialSaleValue, tokenJSON);
 
-        savedTitles[tokenId] = Title(name, Jurisdiction, initialSaleValue, tokenJSON);
-
-        return tokenId;
+        return newItemId;
     }
+
+    function totalSupply () public view override returns (uint256) {
+        return _tokenIds.current();
+    }
+    
+    
 
     function newSale(
         uint256 tokenId,
@@ -54,5 +87,39 @@ contract TitleRegistry is ERC721Full {
         emit Sale(tokenId, newSaleValue, reportURI, tokenJSON);
 
         return (savedTitles[tokenId].saleValue);
+    }
+
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+     function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
